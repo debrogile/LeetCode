cf0aa77d8e4790dbec84ee7b01f30a79a5db9f2d
debrogile <debrogile@163.com>
2019-04-22 18:49:55 +0800

    [107][C#]新增

---
+++ C#/107.二叉树的层次遍历-ii.cs
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=csharp
+ *
+ * [107] 二叉树的层次遍历 II
+ *
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * algorithms
+ * Easy (59.71%)
+ * Total Accepted:    13.4K
+ * Total Submissions: 22.5K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * 
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 返回其自底向上的层次遍历为：
+ * 
+ * [
+ * ⁠ [15,7],
+ * ⁠ [9,20],
+ * ⁠ [3]
+ * ]
+ * 
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+using System.Collections.Generic;
+
+public class Solution
+{
+    public IList<IList<int>> LevelOrderBottom(TreeNode root)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        var list = new List<List<int>>();
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var level = new List<int>();
+            for (int i = queue.Count; i > 0; i--)
+            {
+                var node = queue.Dequeue();
+                level.Add(node.val);
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+
+            list.Insert(0, level);
+        }
+
+        return list;
+    }
+}
