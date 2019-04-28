62e7f74a8f31d8261457ba7fe0115b8f778dfb89
debrogile <debrogile@163.com>
2019-04-28 15:50:42 +0800

    [168][C#]新增

---
+++ C#/168.excel表列名称.cs
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=168 lang=csharp
+ *
+ * [168] Excel表列名称
+ *
+ * https://leetcode-cn.com/problems/excel-sheet-column-title/description/
+ *
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ *
+ * 例如，
+ *
+ * 1 -> A
+ * 2 -> B
+ * 3 -> C
+ * ...
+ * 26 -> Z
+ * 27 -> AA
+ * 28 -> AB 
+ * ...
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "A"
+ *
+ *示例 2:
+ *
+ * 输入: 28
+ * 输出: "AB"
+ *
+ * 示例 3:
+ *
+ * 输入: 701
+ * 输出: "ZY"
+ *
+ */
+using System.Text;
+
+public class Solution
+{
+    public string ConvertToTitle(int n)
+    {
+        var sb = new StringBuilder();
+        while (n > 0)
+        {
+            int temp = n % 26;
+            n /= 26;
+
+            char c = (char) (temp + 64);
+            if (temp == 0)
+            {
+                c = 'Z';
+                n--;
+            }
+
+            sb.Insert(0, c);
+        }
+
+        return sb.ToString();
+    }
+}
