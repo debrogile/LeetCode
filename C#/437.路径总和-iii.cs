620dba417050fe7e1d72b425ef2389e982b6d8b4
debrogile <debrogile@163.com>
2019-06-03 16:14:38 +0800

    [437][C#]新增

---
+++ C#/437.路径总和-iii.cs
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode.cn id=437 lang=csharp
+ *
+ * [437] 路径总和 III
+ *
+ * https://leetcode-cn.com/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Easy (49.27%)
+ * Likes:    143
+ * Dislikes: 0
+ * Total Accepted:    5.6K
+ * Total Submissions: 11.3K
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * 给定一个二叉树，它的每个结点都存放着一个整数值。
+ * 
+ * 找出路径和等于给定数值的路径总数。
+ * 
+ * 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ * 
+ * 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+ * 
+ * 示例：
+ * 
+ * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ * 
+ * ⁠     10
+ * ⁠    /  \
+ * ⁠   5   -3
+ * ⁠  / \    \
+ * ⁠ 3   2   11
+ * ⁠/ \   \
+ * 3  -2   1
+ * 
+ * 返回 3。和等于 8 的路径有:
+ * 
+ * 1.  5 -> 3
+ * 2.  5 -> 2 -> 1
+ * 3.  -3 -> 11
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public int PathSum(TreeNode root, int sum)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        return PathSumA(root, sum);
+    }
+
+    private int PathSumA(TreeNode node, int sum)
+    {
+        int count = 0;
+        if (node.val == sum)
+        {
+            count++;
+        }
+
+        int diff = sum - node.val;
+        if (node.left != null)
+        {
+            count += PathSumA(node.left, sum);
+            count += PathSumB(node.left, diff);
+        }
+
+        if (node.right != null)
+        {
+            count += PathSumA(node.right, sum);
+            count += PathSumB(node.right, diff);
+        }
+
+        return count;
+    }
+
+    private int PathSumB(TreeNode node, int sum)
+    {
+        int count = 0;
+        if (node.val == sum)
+        {
+            count++;
+        }
+
+        int diff = sum - node.val;
+        if (node.left != null)
+        {
+            count += PathSumB(node.left, diff);
+        }
+
+        if (node.right != null)
+        {
+            count += PathSumB(node.right, diff);
+        }
+
+        return count;
+    }
+}
